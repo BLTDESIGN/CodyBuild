a0f4d002987cfb95794350c159e9e07e5b1b0269
BLTDESIGN <beltramo.dan@gmail.com>
2020-10-28 08:47:38 -0700

    Updated About Me page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bddca93719d951b369d7d67b8078fcb3.js"
+  "/precache-manifest.24615b72754d1e98acaf75cc0c3de7b9.js"
 );
 
 self.addEventListener('message', (event) => {
